0c4c0f235b86f9108e5265cc3d1bede20c819331
xuzengqiang <253948113@qq.com>
2018-05-24 23:55:32 +0800

    优化目录结构

---
+++ components/builder.js
@@ -0,0 +1,10 @@
+/**
+ * @fileOverview json构建组件
+ * @author xuzengqiang <253948113@qq.com>
+ * @date 2018-05-24 11:45:39
+ * @version 1.0.0
+ */
+const Builder = {
+    template: '#builder-template',
+    data() {}
+}
